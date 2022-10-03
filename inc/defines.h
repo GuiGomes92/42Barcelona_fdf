1c91513ae258d3f61000fe75aecab554996a279a
Guilherme Braga gomes <gbraga-g@cbr13s5.42barcelona.com>
2022-10-03 18:51:07 +0200

    Created defines for points

--- inc/defines.h
+++ inc/defines.h
@@ -3,6 +3,20 @@
 
 #define WINX 1600
 #define WINY 800
+
+typedef struct s_pointY
+{
+    int x1;
+    int x2
+} t_pointY;
+
+typedef struct s_pointX
+{
+    int x1;
+    int x2
+} t_pointX;
+
+
 typedef struct s_vars
 {
     void *mlx_ptr;
